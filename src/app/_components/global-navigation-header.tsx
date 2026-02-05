d71b8cffbed77e336dc2f2996ef61017c4907664
Zebrina <sabrina.bjurman@gmail.com>
2026-02-05 09:05:40 +0100

    wrapped in header tag

--- src/app/_components/global-navigation-header.tsx
+++ src/app/_components/global-navigation-header.tsx
@@ -12,47 +12,49 @@ type Props = Omit<React.ComponentProps<typeof NavigationMenu>, "className"> & {
 
 export default function GlobalNavigationHeader({ className }: Props) {
     return (
-        <NavigationMenu className={className}>
-            <h1 className="mr-4 p-1 border-2 self-center border-pink-500 bg-pink-300 rounded-xl">
-                A+ MovieShop (PLACEHOLDER LOGO)
-            </h1>
-            <NavigationMenuList>
-                <NavigationMenuItem>
-                    <NavigationMenuLink
-                        asChild
-                        className="navigationMenuTriggerStyle()">
-                        <Link href="/">Home</Link>
-                    </NavigationMenuLink>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <NavigationMenuLink
-                        asChild
-                        className="navigationMenuTriggerStyle()">
-                        <Link href="/movies">Movies</Link>
-                    </NavigationMenuLink>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <NavigationMenuLink
-                        asChild
-                        className="navigationMenuTriggerStyle()">
-                        <Link href="/orders">Orders</Link>
-                    </NavigationMenuLink>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <NavigationMenuLink
-                        asChild
-                        className="navigationMenuTriggerStyle()">
-                        <Link href="/cart">Cart</Link>
-                    </NavigationMenuLink>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <NavigationMenuLink
-                        asChild
-                        className="navigationMenuTriggerStyle()">
-                        <Link href="/about">About</Link>
-                    </NavigationMenuLink>
-                </NavigationMenuItem>
-            </NavigationMenuList>
-        </NavigationMenu>
+        <header className={className}>
+            <NavigationMenu>
+                <h1 className="mr-4 p-1 border-2 self-center border-pink-500 bg-pink-300 rounded-xl">
+                    A+ MovieShop (PLACEHOLDER LOGO)
+                </h1>
+                <NavigationMenuList>
+                    <NavigationMenuItem>
+                        <NavigationMenuLink
+                            asChild
+                            className="navigationMenuTriggerStyle()">
+                            <Link href="/">Home</Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <NavigationMenuLink
+                            asChild
+                            className="navigationMenuTriggerStyle()">
+                            <Link href="/movies">Movies</Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <NavigationMenuLink
+                            asChild
+                            className="navigationMenuTriggerStyle()">
+                            <Link href="/orders">Orders</Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <NavigationMenuLink
+                            asChild
+                            className="navigationMenuTriggerStyle()">
+                            <Link href="/cart">Cart</Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <NavigationMenuLink
+                            asChild
+                            className="navigationMenuTriggerStyle()">
+                            <Link href="/about">About</Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+        </header>
     );
 }
