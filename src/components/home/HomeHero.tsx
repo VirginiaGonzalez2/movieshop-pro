c090b7952cd3cd147c6af022fb7d345351888a6f
Håkan Bengtsson <hakan_be_privat@hotmail.com>
2026-02-24 16:30:43 +0100

    Initial home page components

---
+++ src/components/home/HomeHero.tsx
@@ -0,0 +1,11 @@
+export default function HomeHero()
+{
+  return (
+    <div>
+      <div>
+        <h1 className="text-2xl font-bold mb-4">Home Hero</h1>
+        <p>Hero hero</p>
+      </div>
+    </div>
+  );
+}
