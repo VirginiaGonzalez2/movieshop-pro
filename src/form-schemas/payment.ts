53a5a8a4012188309fb1c05bab70f137805aa1ac
Sabrina <sabrina.bjurman@gmail.com>
2026-02-27 10:35:02 +0100

    Card payment errors.

--- src/form-schemas/payment.ts
+++ src/form-schemas/payment.ts
@@ -16,26 +16,33 @@ export type PaymentMethod = "card" | "paypal";
 
 // Card
 
-const errCardNameMissing = "Please enter the name found on your card.";
+const errCardNameMissing = "Please enter the name found on the back of your card.";
 const errCardNumberMissing = "Please enter your card number.";
 const errCardNumberInvalid =
     "This is not a valid card number." +
     (development ? " [DEV: Start with 0 to decline the card on processing]" : "");
 const errCardSecurityCodeInvalid =
     "Please enter the 3-digit security code found on the back of your card.";
+const errCardExpirationDateMissing = "Please enter the expiration date of your card.";
+const errCardExpirationDateInvalid = "This is not a valid expiration date.";
 
-export const paymentCardSchema = z.object({
-    cardName: z.string(errCardNameMissing),
-    cardNumber: z
-        .string(errCardNumberMissing)
-        .min(16, errCardNumberInvalid)
-        .max(16, errCardNumberInvalid),
-    cardSecurityCode: z
-        .string(errCardSecurityCodeInvalid)
-        .min(3, errCardSecurityCodeInvalid)
-        .max(3, errCardSecurityCodeInvalid),
-    cardExpirationDate: z.string(),
-});
+export const paymentCardSchema = z
+    .object({
+        cardName: z.string(errCardNameMissing),
+        cardNumber: z
+            .string(errCardNumberMissing)
+            .min(16, errCardNumberInvalid)
+            .max(16, errCardNumberInvalid),
+        cardSecurityCode: z
+            .string(errCardSecurityCodeInvalid)
+            .min(3, errCardSecurityCodeInvalid)
+            .max(3, errCardSecurityCodeInvalid),
+        cardExpirationDate: z.string(errCardExpirationDateMissing),
+    })
+    .refine((values) => !!values.cardExpirationDate.match(/[01]\d{5}/), {
+        error: errCardExpirationDateInvalid,
+        path: ["cardExpirationDate"],
+    });
 
 export type PaymentCardFormValues = z.infer<typeof paymentCardSchema>;
 
