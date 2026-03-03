a4c3f32143b169f3007359df063e62700134af7d
Sabrina <sabrina.bjurman@gmail.com>
2026-03-03 14:20:54 +0100

    User roles with some order permissions

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,14 +1,23 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { admin } from "better-auth/plugins";
+import { admin as adminPlugin } from "better-auth/plugins";
 import { prisma } from "./prisma";
+import { ac, admin, user } from "./auth-role-permissions";
 
 // Added this to solve some issues I have personally ran into
 // on a separate project. /Sabrina
 const development = process.env.NODE_ENV == "development";
 
 export const auth = betterAuth({
-    plugins: [admin()],
+    plugins: [
+        adminPlugin({
+            ac,
+            roles: {
+                admin,
+                user,
+            },
+        }),
+    ],
 
     database: prismaAdapter(prisma, {
         provider: "postgresql",
