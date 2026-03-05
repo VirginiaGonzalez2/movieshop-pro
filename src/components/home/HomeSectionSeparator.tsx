2b76ab34d3d6e8c3fbcf12ab3010ba00e747895c
Håkan Bengtsson <hakan_be_privat@hotmail.com>
2026-03-05 09:30:31 +0100

    Added Hero section, HomeSectionSeparator and all wide Hero pictures

--- src/components/home/HomeSectionSeparator.tsx
+++ src/components/home/HomeSectionSeparator.tsx
@@ -11,36 +11,30 @@ export default function HomeSectionSeparator({ genre }: { genre?: string | null 
     console.log("HomeSectionSeparator - selected genre:", genre);
     return (
         <div>
-            <div className="mx-auto max-w-2xl py-8 px-4">
-                <h1 className="text-2xl font-bold mb-4">Hero section</h1>
-                <p className="mb-2">Browse and buy your favorite movies.</p>
+            <div className="mx-auto max-w-2xl pt-22 pb-22">
                 <HomeHero />
             </div>
-
-            
-
-            
-
+         
             {/* Home quick category navigation section */}
             <HomeQuickFilters />
 
             <div className="mb-8">
-                <h1 className="text-left text-2xl font-bold mb-4">Top 5 most recent</h1>
+                <h1 className="text-left text-2xl font-semibold mb-4">Top 5 most recent</h1>
                 <TopRecentMoviesSection genre={genre} />
             </div>
 
             <div className="mb-8">
-                <h1 className="text-left text-2xl font-bold mb-4">Top 5 most purchased</h1>
+                <h1 className="text-left text-2xl font-semibold mb-4">Top 5 most purchased</h1>
                 <TopPurchasedMoviesSection genre={genre} />
             </div>
 
             <div className="mb-8">
-                <h1 className="text-left text-2xl font-bold mb-4">Top 5 oldest</h1>
+                <h1 className="text-left text-2xl font-semibold mb-4">Top 5 oldest</h1>
                 <TopOldestMoviesSection genre={genre} />
             </div>
 
             <div className="mb-8">
-                <h1 className="text-left text-2xl font-bold mb-4">Top 5 cheapest</h1>
+                <h1 className="text-left text-2xl font-semibold mb-4">Top 5 cheapest</h1>
                 <TopCheapestMoviesSection genre={genre} />
             </div>
 
